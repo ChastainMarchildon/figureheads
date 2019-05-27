b619107e6415bd3c6b0c91f22afe6853a3c1bd35
Chastain Marchildon <chastainrgm@gmail.com>
2019-05-27 19:25:51 -0400

    stripe test

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,12 +1,29 @@
-(function() {
-    var a = $('#banner-container').children();
-    var index = 0;
-    run()
-  
-    function run() {
-      a.filter('.active').fadeOut(500).removeClass('active');
-      a.eq(index).fadeIn(500).addClass('active');
-      index = (index + 1) % a.length; // Wraps around if it hits the end
-      setTimeout(run, 1000);
-    }
-  })();
+
+function stripeCheckout(){
+  const stripe = Stripe('pk_test_KtoZqH7idkT9anePS9XkRk7D');
+  console.log('here');
+  //const checkoutButton = document.getElementById('checkout-button-plan_F99sURKqZZKMVs');
+  //checkoutButton.addEventListener('click', function () {
+    // When the customer clicks on the button, redirect
+    // them to Checkout.
+    console.log('here');
+    stripe.redirectToCheckout({
+      items: [{plan: 'plan_F99sURKqZZKMVs', quantity: 1}],
+
+      // Do not rely on the redirect to the successUrl for fulfilling
+      // purchases, customers may not always reach the success_url after
+      // a successful payment.
+      // Instead use one of the strategies described in
+      // https://stripe.com/docs/payments/checkout/fulfillment
+      successUrl: 'https://localhost:3000/listings',
+      cancelUrl: 'https://your-website.com/canceled',
+    })
+    .then(function (result) {
+      if (result.error) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer.
+        var displayError = document.getElementById('error-message');
+        displayError.textContent = result.error.message;
+      } 
+    });
+}
